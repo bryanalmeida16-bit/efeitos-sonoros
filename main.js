2e18b99a26ac4c74932cde6ec67354a150e2e1df
bryanalmeida16-bit <bryan.almeida16@escola.pr.gov.br>
2025-12-02 20:13:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 function tocaSom(idElementoaudio){
-const elemento=Document.QuerySelector(idElementoaudio)
-if( elemento && elemento.localName=== 'audio'){
-    elemento.play ( ); 
+const elemento=document.querySelector(idElementoaudio)
+if( elemento && elemento.localName === 'audio'){
+    elemento.play (); 
 }else{
-    console.log('')
+    console.log('Elemento não encontrado invalido')
 }
 }
+const listaDeTeclas = document.querySelectorAll(".tecla")
+
+
+
+
+for(let contador=o; contador<listaDeTeclas.length;contador++)
+    const tecla=listaDeTeclas[contador];
+    const efeito=tecla.classList[1];
+    const idAudio='#som_${efeito}';
+    tecla.onclick=function(){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown=function(evento){
+        if(EventCounts.code==='Space')
+            tecla.classList.add('ativa');
+    }
+    
+        tecla.onkeydown=function(evento){
+         if(evento.code=== 'Enter')
+         
+            tecla.classList,add('ativa');
+        }
